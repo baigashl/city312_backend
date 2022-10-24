b32aafd0609eb0122843f39d261dcb90348ecd7f
Your Name <you@example.com>
2022-10-25 00:10:43 +0600

    Activity_type_and_Category_CRAD_and_tests

--- apps/activity_type/urls.py
+++ apps/activity_type/urls.py
@@ -3,10 +3,16 @@ from .views import (ActivityTypeListAPIView,
                     ActivityTypeDetailAPIView,
                     ActivityTypeCreateAPIView,
                     CategoryListAPIView,
+                    CategoryCreateAPIView,
                     CategoryDetailAPIView)
 
 
 urlpatterns = [path('list/', ActivityTypeListAPIView.as_view(), name='activity_type_list'),
                path('create/', ActivityTypeCreateAPIView.as_view(), name='activity_type_create'),
                path('detail/<int:id>', ActivityTypeDetailAPIView.as_view(), name='activity_type_detail'),
-               ]
+
+
+               path('cat_list/', CategoryListAPIView.as_view(), name='category_list'),
+               path('cat_create/', CategoryCreateAPIView.as_view(), name='category_create'),
+               path('cat_detail/<int:id>', CategoryDetailAPIView.as_view(), name='category_detail'),
+]
