037de24b5cb6842946fea5d79672686cb37b6ee3
Your Name <you@example.com>
2023-01-28 16:00:50 +0600

    cors

--- city312_backend/settings.py
+++ city312_backend/settings.py
@@ -28,7 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['18.181.228.1', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
     "http://localhost:3000",
+    "http://127.0.0.1:3000/",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
