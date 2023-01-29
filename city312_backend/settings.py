c03c8319e47d3b480182cf1118ebc4de68a630c4
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-01-29 20:45:32 +0600

    rewrite multiple image code

--- city312_backend/settings.py
+++ city312_backend/settings.py
@@ -142,12 +142,6 @@ AUTH_PASSWORD_VALIDATORS = [
 from datetime import timedelta
 
 REST_FRAMEWORK = {
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
-    ),
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
