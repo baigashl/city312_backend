4420c7796e784f0a9cf8f71b08c4b81873a0844c
Medet <medik121@gmail.com>
2023-02-03 19:49:38 +0600

    implement CRUD nested serializer logic in users

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -6,32 +6,34 @@ from apps.users.models import (
 )
 
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    """Serializer for user model registration"""
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for User model registration"""
 
     class Meta:
         model = User
         exclude = [
             'groups',
             'user_permissions',
-            'is_superuser',
         ]
         read_only_fields = [
             'is_active',
             'is_staff',
             'last_login',
+            'is_superuser',
         ]
 
 
 class ClientProfileSerializer(serializers.ModelSerializer):
+    """Serializer for ClientProfile model"""
 
-    user = RegisterUserSerializer()
+    user = UserSerializer()
 
     class Meta:
         model = ClientProfile
         fields = '__all__'
 
     def create(self, validated_data):
+        """Create nested UserSerializer fields"""
 
         user_data = validated_data.pop('user')
         password_data = list(user_data.values())[0]
@@ -42,16 +44,29 @@ class ClientProfileSerializer(serializers.ModelSerializer):
         client.save()
         return client
 
+    def update(self, instance, validated_data):
+        """Update nested UserSerializer fields """
+
+        if 'user' in validated_data:
+            nested_serializer = self.fields['user']
+            nested_instance = instance.user
+            nested_data = validated_data.pop('user')
+            nested_serializer.update(nested_instance, nested_data)
+        return super(ClientProfileSerializer, self).update(instance, validated_data)
+
 
 class PartnerProfileSerializer(serializers.ModelSerializer):
+    """Serializer for PartnerProfile model"""
 
-    user = RegisterUserSerializer()
+    user = UserSerializer()
 
     class Meta:
         model = PartnerProfile
         fields = '__all__'
 
     def create(self, validated_data):
+        """Create nested UserSerializer fields"""
+
         user_data = validated_data.pop('user')
         password_data = list(user_data.values())[0]
         user = User.objects.create(**user_data)
@@ -60,6 +75,16 @@ class PartnerProfileSerializer(serializers.ModelSerializer):
         client = PartnerProfile.objects.create(user=user, **validated_data)
         client.save()
         return client
+
+    def update(self, instance, validated_data):
+        """Update nested UserSerializer fields """
+
+        if 'user' in validated_data:
+            nested_serializer = self.fields['user']
+            nested_instance = instance.user
+            nested_data = validated_data.pop('user')
+            nested_serializer.update(nested_instance, nested_data)
+        return super(PartnerProfileSerializer, self).update(instance, validated_data)
 
 
 class LoginUserSerializer(serializers.ModelSerializer):
