63f96adcd7a0a9778c620c88f31c9c065b77e3ec
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-02-04 14:19:34 +0600

    add cart urls

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import CartViewSet
+from .views import CartViewSet, CartDiscountViewSet
 
 router = routers.DefaultRouter()
+router.register(r'cart_item', CartDiscountViewSet)
 router.register(r'carts', CartViewSet)
+# router.register(r'payment', PaymentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
