4991e6b4246a2ce1c1cb3e00a42874eefeeea49a
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-02-04 14:19:41 +0600

    add cart views

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,9 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
-from apps.cart.models import Cart
-from apps.cart.serializers import CartSerializer
+from apps.cart.models import Cart, CartDiscount
+from apps.cart.serializers import CartSerializer, CartDiscountSerializer
+
+
+class CartDiscountViewSet(viewsets.ModelViewSet):
+    queryset = CartDiscount.objects.all()
+    serializer_class = CartDiscountSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    permission_classes = [permissions.AllowAny]
+
+# class PaymentViewSet(viewsets.ModelViewSet):
+#     queryset = Payment.objects.all()
+#     serializer_class = PaymentSerializer
