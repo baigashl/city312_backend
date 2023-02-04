04bcda49a048d03033e0b7b41431b7d0ca62e5e7
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-02-04 14:19:10 +0600

    new cart

--- apps/cart/serializers.py
+++ apps/cart/serializers.py
@@ -1,26 +1,28 @@
 from rest_framework import serializers
 
-from .models import Cart, CartItem
+from .models import Cart, CartDiscount
 
 
-class CartItemSerializer(serializers.ModelSerializer):
+class CartDiscountSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CartItem
+        model = CartDiscount
         fields = '__all__'
 
 
 class CartSerializer(serializers.ModelSerializer):
-    cart_items = CartItemSerializer(many=True)
-
     class Meta:
         model = Cart
         fields = '__all__'
+        read_only_fields = ["total_price"]
 
-    def create(self, validated_data):
-        cart_items_data = validated_data.pop('cart_items', [])
-        cart = Cart.objects.create(**validated_data)
-
-        for item in cart_items_data:
-            CartItem.objects.create(cart=cart, **item)
-
-        return cart
+# class PaymentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Payment
+#         fields = '__all__'
+#
+#     def create(self, validated_data):
+#         payment = Payment.objects.create(**validated_data)
+#         cart = self.context['cart']
+#         email = self.context['email']
+#         payment.charge(cart, email)
+#         return payment
