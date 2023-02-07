2928573876460adc1a5f99ac1c40fc819bd781cd
Your Name <you@example.com>
2023-02-08 00:17:12 +0600

    ssl

--- init_letsencript.sh
+++ init_letsencript.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(baigashl.online www.baigashl.online)
+domains=(18.181.228.1)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
