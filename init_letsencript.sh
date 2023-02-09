92df72f10d43b909fc575ef6202c171ae2585479
Your Name <you@example.com>
2023-02-09 12:57:13 +0600

    add www

--- init_letsencript.sh
+++ init_letsencript.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(312backend.com)
+domains=(312backend.com www.312backend.com)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
