b0ca3d1d9383bd9040edc9a976bb050f3a3eae9d
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2023-02-11 21:26:23 +0600

    new captcha

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
 
-from apps.users.recaptcha_verify import verify_recaptcha
+# from apps.users.recaptcha_verify import verify_recaptcha
 from apps.users.views import (
     LoginView,
     ClientProfileView,
@@ -19,7 +19,7 @@ router.register('register/partner', PartnerProfileView)
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
-    path('recaptcha/verify/', verify_recaptcha, name='verify_recaptcha'),
+    # path('recaptcha/verify/', verify_recaptcha, name='verify_recaptcha'),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
